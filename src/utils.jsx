d12bf23199524a308625c3a17d4e828589f5cb6e
votsa <votsa@afkxxx.com>
2017-04-11 14:49:14 +0300

    Utils.js fix

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable */
 
 // Closest polyfil
-if (Element && !Element.prototype.closest) {
-  Element.prototype.closest = function closest(s) {
-    const matches = (this.document || this.ownerDocument).querySelectorAll(s);
-    let i;
-    let el = this;
+(function(global) {
+  if (global.Element && !global.Element.prototype.closest) {
+    Element.prototype.closest = function closest(s) {
+      const matches = (this.document || this.ownerDocument).querySelectorAll(s);
+      let i;
+      let el = this;
 
-    do {
-      i = matches.length;
-      while (--i >= 0 && matches.item(i) !== el) {}
-    } while ((i < 0) && (el = el.parentElement));
+      do {
+        i = matches.length;
+        while (--i >= 0 && matches.item(i) !== el) {}
+      } while ((i < 0) && (el = el.parentElement));
 
-    return el;
-  };
-}
+      return el;
+    };
+  }
+})(typeof window === 'undefined' ? this : window);
 
 // Ignore case and trim whitespaces
 export const ignoreCase = value => value.toUpperCase().trim();
