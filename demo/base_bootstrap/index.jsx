77f4bb614719f5152f6e8bfd06d3135aa6db7fd8
votsa <votsa@afkxxx.com>
2017-04-11 11:08:34 +0300

    Added bootstrap and material demos

---
+++ demo/base_bootstrap/index.jsx
@@ -0,0 +1,72 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BootstrapContainer } from 'react-table-components';
+
+import 'styles/styles';
+
+const UserPic = (row) => (
+  <div className="text-center">
+    <img src={row.pic} />
+  </div>
+);
+
+const EditBtn = (row) => (
+  <div className="text-center">
+    <button className="btn btn-xs btn-success">Edit</button>
+  </div>
+);
+
+const generateRowProps = row => {
+  const options = {};
+
+  if (row.gender === 'Male') {
+    options.className = 'info';
+  }
+
+  if (row.gender === 'Female') {
+    options.className = 'warning';
+  }
+
+  return options;
+};
+
+function BaseTable(data) {
+  const columns = [
+    { id: 1, title: 'Avatar', render: UserPic, visible: false, width: '50px' },
+    { id: 2, title: 'First name', prop: 'first_name', width: '150px' },
+    { id: 3, title: 'Last name', prop: 'last_name', width: '150px' },
+    { id: 4, title: 'Email', prop: 'email' },
+    { id: 5, title: 'Gender', prop: 'gender', width: '85px' },
+    { id: 6, title: 'Ip address', prop: 'ip_address', width: '150px' },
+    { id: 7, title: 'Country', prop: 'country.name', width: '150px' },
+    { id: 8, title: 'Code', prop: 'country.code', width: '80px' },
+    { id: 9, title: 'City', prop: 'city.name' },
+    { id: 10, title: 'Action', render: EditBtn, width: '50px' },
+  ];
+
+  return (
+    <div className="container">
+      <br />
+      <BootstrapContainer
+        keys="id"
+        className="table table-bordered table-striped"
+        columns={columns}
+        onDragColumn={columns => console.log(columns)}
+        onChangeColumnsVisibility={columns => console.log(columns)}
+        dataArray={data}
+        draggable={true}
+        sortable={true}
+        sortBy={{ prop: 'country.name', order: 'asc' }}
+        generateRowProps={generateRowProps}
+        pageSizeOptions={[5, 10, 50, 100]}
+      />
+    </div>
+  );
+}
+
+fetch('/data.json')
+  .then(res => res.json())
+  .then((rows) => {
+    ReactDOM.render(BaseTable(rows), document.getElementById('app'));
+  });
---
+++ demo/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  devServer: {
+    contentBase: __dirname,
+  },
+  entry: {
+    base_bootstrap: './base_bootstrap/index.jsx',
+    material: './material/index.jsx',
+  },
+  output: {
+    filename: '[name].entry.js',
+  },
+  module: {
+  rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          query: {
+            cacheDirectory: true,
+          }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract({
+        fallbackLoader: 'style-loader',
+        loader: [{
+            loader: 'css-loader'
+          }],
+      })
+    }
+  ]},
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    alias: {
+      'react-table-components': path.join(__dirname, '../src'),
+      'styles': path.join(__dirname, '../styles'),
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css',
+    }),
+  ]
+};
